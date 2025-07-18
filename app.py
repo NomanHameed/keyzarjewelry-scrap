a236d759b51bb3cf2aaf04ecb9f0eee4327c2399
noman hameed <nomanbutt8322@gmail.com>
2025-07-18 16:58:24 +0500

    update port

--- app.py
+++ app.py
@@ -139,5 +139,9 @@ def scrape():
 
     return jsonify({'message': 'Scraping complete', 'csv': CSV_FILE, 'images_saved': len(image_urls), 'data': data})
 
+# if __name__ == '__main__':
+#     app.run(debug=True) 
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
