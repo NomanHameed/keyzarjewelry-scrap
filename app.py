b50e066322c463622f3e4a81e9c012d29b52c79c
noman hameed <nomanbutt8322@gmail.com>
2025-07-19 19:22:41 +0500

    add port

--- app.py
+++ app.py
@@ -571,4 +571,5 @@ def scrape_all_pages_stream():
     return Response(generate(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
